db481e3eb38a50eb91dbe6779f75223c73ed7a47
NikuleskuOleh <125149944+NikuleskuOleh@users.noreply.github.com>
2023-05-05 19:15:38 +0300

    Update fizz_buzz.py

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,10 @@
 def fizz_buzz(param):
   #Type your code here
-  return
+  if param % 3 == 0 and param % 5 == 0:
+        return "FizzBuzz"
+    elif param % 3 == 0:
+        return "Fizz"
+    elif param % 5 == 0:
+        return "Buzz"
+    else:
+        return str(param)
